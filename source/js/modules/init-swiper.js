7d85199c4c46e55767181fc14dde9547e9fbd014
Юрий <urijsaznev840@gmail.com>
2023-05-15 22:29:40 +0500

    Fixed

--- source/js/modules/init-swiper.js
+++ source/js/modules/init-swiper.js
@@ -17,18 +17,13 @@ const initStaffSwiper = () => {
       // when window width is >= 768px
       768: {
         slidesPerView: 2,
-        slidesPerGroup: 2,
+        slidesPerGroup: 1,
         spaceBetween: 30,
       },
       // when window width is >= 1200px
-      1200: {
-        slidesPerView: 3,
-        slidesPerGroup: 1,
-      },
-      // when window width is >= 1200px
-      1280: {
+      1366: {
         slidesPerView: 4,
-        slidesPerGroup: 4,
+        slidesPerGroup: 1,
       },
     },
   });
